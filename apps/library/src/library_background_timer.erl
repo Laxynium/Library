63ce3a8ae9ab3402a74eaeebd72ff1b37ad828a4
Grzegorz Gruszczyk <grzesiu15432@gmail.com>
2020-01-16 00:50:42 +0100

    Add periodical update if user can borrow book

---
+++ apps/library/src/library_background_timer.erl
@@ -0,0 +1,32 @@
+-module(library_background_timer).
+
+-include_lib("core/src/core_book.hrl").
+
+-behaviour(gen_server).
+
+-export([start_link/1, init/1, handle_info/2, handle_cast/2, handle_call/3]).
+
+start_link({Period, DataProcessName, Now}) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, {Period, DataProcessName, Now}, []).
+
+init({Period, DataProcessName, Now}) ->
+    Timer = erlang:send_after(1, self(), update),
+    {ok, {Period, DataProcessName, Now, Timer}}.
+
+handle_cast(_,_)->ok.
+handle_call(_,_,_)->{reply,{}}.
+
+handle_info(update, {Period, DataProcessName, Now, OldTimer}) ->
+    erlang:cancel_timer(OldTimer),
+    DataProcessId = erlang:whereis(DataProcessName),
+    DataProcessId ! {update_if_user_can_rent, self(), get, all_books},
+
+    Timer = erlang:send_after(Period, self(), update),
+    {noreply, {Period, DataProcessName, Now, Timer}};
+
+handle_info({update_if_user_can_rent, result, AllBooks}, {Period, DataProcessName, Now, OldTimer}) ->
+    OverdueBooks = lists:filter(fun (B) -> core_book:is_overdue(B, Now) end, AllBooks),
+    OverdueStudents = lists:map(fun ({_,_,[{_,_,By,_,_}|_]})-> By end, OverdueBooks),
+    DataProcessId = erlang:whereis(DataProcessName),
+    DataProcessId ! {update_if_user_can_rent, result, OverdueStudents},
+    {noreply, {Period, DataProcessName, Now, OldTimer}}.
