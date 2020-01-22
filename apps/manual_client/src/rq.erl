05a7ae7ab3851420247108f7a36f15694a08540d
GloomyC <ponuryksych@o2.pl>
2020-01-22 19:39:02 +0100

    IT WOOORKS

--- apps/manual_client/src/rq.erl
+++ apps/manual_client/src/rq.erl
@@ -9,7 +9,7 @@ getAllBooks() ->
         2000 -> timeout
     end.
 getAllClients() -> 
-    gen_server:cast(library_server_man,{query,all_clients,self(),nodata}),
+    gen_server:cast(global:whereis_name(library_server_man),{query,all_clients,self(),nodata}),
     receive
         Response -> Response
     after 
